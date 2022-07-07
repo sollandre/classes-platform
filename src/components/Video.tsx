e93926dd10ddd903ac8209d13138c0da5c84655e
sollandre <andrebmsolla@gmail.com>
2022-07-06 21:12:14 -0300

    GraphQL queries and mutations refactores using Codegen

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,51 +9,23 @@ import {
 
 import '@vime/core/themes/default.css'
 import { VideoSkeleton } from "./VideoSkeleton";
-
-const GET_LESSON_BY_SLUG = gql`
-  query GetLessonBySLug($slug: String) {
-  lesson(where: {slug: $slug}) {
-    id
-    title
-    videoId
-    description
-    teacher {
-      bio
-      avatarURL
-      name
-    }
-  }
-}
-`
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
   lessonSlug: string;
 }
 
-interface GetLessonBySlugResponse {
-  lesson: {
-    id: string
-    title: string
-    videoId: string
-    description: string
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    }
-  }
-}
+
 
 export function Video(props: VideoProps ) {
 
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
-    }
-  })
+    } 
+  });
 
-
-  if(!data){
+  if(!data || !data.lesson){
     return (
       <div className="flex-1 animate">
         <VideoSkeleton />
@@ -82,25 +54,28 @@ export function Video(props: VideoProps ) {
             <p className="mt-4 text-gray-200 leading-relaxed text-sm lg:text-base">
               {data.lesson.description}
             </p>
+            {
+              data.lesson.teacher && (
+                <div className="flex items-center gap-4 mt-6">
+                  <img
+                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                    src={data.lesson.teacher.avatarURL}
+                    alt="Avatar do professor da aula"
+                  />
+                  
+                  <div className="leading-relaxed">
+                    <strong className="font-bold text-2xl block">
+                      {data.lesson.teacher.name}
+                    </strong>
+                    <span className="text-gray-200 text-sm block mt-2">
+                      {data.lesson.teacher.bio}
+                    </span>
+                  </div>
+              
+                </div>
+              )
+            }
             
-            <div className="flex items-center gap-4 mt-6">
-              
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lesson.teacher.avatarURL}
-                alt="Avatar do professor da aula"
-              />
-              
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">
-                  {data.lesson.teacher.name}
-                </strong>
-                <span className="text-gray-200 text-sm block mt-2">
-                  {data.lesson.teacher.bio}
-                </span>
-              </div>
-          
-            </div>
           </div>
 
           <div className="flex flex-col gap-4 mt-8 lg:mt-0">
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,14 +2,15 @@ import { gql, useMutation } from "@apollo/client";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
-const CREATE_SUBSCRIBER_MUTATION = gql`
-  mutation CreateSubscriber($name: String!, $email:String!){
-    createSubscriber(data: {name: $name, email: $email}){
-      id
-    }
-  }
-`
+// const CREATE_SUBSCRIBER_MUTATION = gql`
+//   mutation CreateSubscriber($name: String!, $email:String!){
+//     createSubscriber(data: {name: $name, email: $email}){
+//       id
+//     }
+//   }
+// `
 
 export function Subscribe() {
 
@@ -18,7 +19,8 @@ export function Subscribe() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
 
-  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation()
+  // useMutation(CREATE_SUBSCRIBER_MUTATION)
 
   async function handleSubscribe(event: FormEvent){
     event.preventDefault();
